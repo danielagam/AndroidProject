207afc06ddb00ba65f56d040c9a59e261df0b79e
danielag <danielrahimi0@gmail.com>
2018-10-30 16:22:33 +0200

    הוספת החיבור הראשוני לשרת
    שם משתמש: shimonanddaniel@gmail.com
    סיסמא: Java2018
    
    נבדקה הפעלת הפונקציה getInstance() (כעת בהערה) והיא אכן רושמת נתונים בשרת

--- app/src/main/java/com/danielshimon/android_project/model/MainActivity.java
+++ app/src/main/java/com/danielshimon/android_project/model/MainActivity.java
@@ -1,4 +1,5 @@
 package com.danielshimon.android_project.model;
+
 import android.app.TimePickerDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -8,20 +9,39 @@ import android.widget.EditText;
 import android.widget.TimePicker;
 import android.widget.Toast;
 
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
 
 import com.danielshimon.android_project.R;
 import com.danielshimon.android_project.model.entities.Travel;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
     private EditText editText;
     private Button btn;
     private EditText chooseTime;
 
+    void getInstance() {
+        FirebaseDatabase database = FirebaseDatabase.getInstance();
+        DatabaseReference myRef = database.getReference("message");
+        HashMap<String, Object> hashMap = new HashMap();
+        hashMap.put("key-1", "value 1 ");
+        hashMap.put("key-2", "value 2 ");
+        hashMap.put("key-3", "value 3 ");
+        myRef.setValue(hashMap);
+    }
+
+
     Travel travel = new Travel();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
 
+        //getInstance();
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         chooseTime = findViewById(R.id.startDrivingRequest);
@@ -53,4 +73,6 @@ public class MainActivity extends AppCompatActivity {
             }
         });
     }
+
+
 }
